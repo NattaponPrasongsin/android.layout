532f34fcef9b34d4d1c4654e76d1a3770aa6844e
NattaponPrasongsin <Prasongsin.natt@gmail.com>
2020-01-12 19:46:39 +0700

    Discount Calculator App

---
+++ calculator/app/src/main/java/th/ac/su/nattapon/calculator/MainActivity.kt
@@ -0,0 +1,43 @@
+package th.ac.su.nattapon.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var edtV1 = findViewById<EditText>(R.id.edtV1)
+        var edtV2 = findViewById<EditText>(R.id.edtV2)
+        var btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        var tvResult = findViewById<TextView>(R.id.tvResult)
+
+        var rgOperator = findViewById<RadioGroup>(R.id.rgOperator)
+
+        var checkbox1 = findViewById<CheckBox>(R.id.checkbox1)
+
+        btnCalculate.setOnClickListener {
+
+            if (checkbox1.isChecked){
+
+                var v1:Int = edtV1.text.toString().toInt()
+                var v2:Int = edtV2.text.toString().toInt()
+                var result = 0
+
+                when(rgOperator.checkedRadioButtonId){
+                    R.id.rbAddition-> result = v1+v2
+                    R.id.rbSubstraction-> result = v1-v2
+                    R.id.rbMultiplication-> result = v1*v2
+                    R.id.rbDivision-> result = v1/v2
+                }
+
+                tvResult.setText(result.toString())
+            }
+
+        }
+
+
+    }
+}
